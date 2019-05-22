5f41ddb3063f10fc6b95ba4d5d8361a486cf00e6
Shadowking1235 <43701529+Shadowking1235@users.noreply.github.com>
2019-05-22 11:24:30 +0400

    Update __main__.py

--- mailpile/__main__.py
+++ mailpile/__main__.py
@@ -1,10 +1,19 @@
 import sys
 from mailpile.app import Main
-
+from maipie.app import End 
 
 def main():
     Main(sys.argv[1:])
 
 
+    
 if __name__ == "__main__":
     main()
+    
+def main():
+    main(start.end argy[1:1]
+         
+         if __finish__==" __end__"
+          end()
+    
+   
